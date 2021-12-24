d2cd105db86ba5086d947601b45e180d3c8cc937
essentonline <essent.online@essent.nl>
2021-12-24 11:53:29 +0100

    first

---
+++ shopware-pwa.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    shopwareEndpoint: "https://ikfiets.shopware.store",
+    shopwareAccessToken: "SWSCVW0XYVR1YTDCBFYZEJB2AA",
+    shopwareDomainsAllowList: "http://localhost:3000",
+  };
