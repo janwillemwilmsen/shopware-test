add87b335b46cb791c35c00ed5cc08a307d5ecf8
essentonline <essent.online@essent.nl>
2021-12-24 14:27:07 +0100

    node12-echt

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import extendNuxtConfig from "@shopware-pwa/nuxt-module/config"
 
 export default extendNuxtConfig({
   head: {
-    title: "Shopware PWA JWWWNode12",
+    title: "Shopware PWA JWWWNode12ecgt",
     meta: [{ hid: "description", name: "description", content: "" }],
   },
 })
