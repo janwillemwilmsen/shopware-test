6b321b95cb574f8d4a690c9a816dd55072ca3c3e
essentonline <essent.online@essent.nl>
2021-12-24 12:50:07 +0100

    bots

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import extendNuxtConfig from "@shopware-pwa/nuxt-module/config"
 
 export default extendNuxtConfig({
   head: {
-    title: "Shopware PWA",
+    title: "Shopware PWA JWW",
     meta: [{ hid: "description", name: "description", content: "" }],
   },
 })
